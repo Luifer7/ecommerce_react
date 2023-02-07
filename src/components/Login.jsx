90628c82e46a587329663c6b14514b302ec52cf6
chrisHuila <christian.huila22@gmail.com>
2023-02-07 17:18:06 -0500

    italic link nav

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ const Login = () => {
       <nav className="navbar navbar-expand pb-3">
         <div className="collapse navbar-collapse" >
           <div className="navbar-nav">
-            <a className="nav-link text-white" href="#">Sign in </a>            
-            <a className="nav-link text-white" href="#">Log in </a>
+            <a className="nav-link text-white fst-italic" href="#">Sign in </a>            
+            <a className="nav-link text-white fst-italic" href="#">Log in </a>
             <a className="nav-link text-white" href="#"><i className="bi bi-cart4">Carrito</i></a>
           </div>
         </div>
