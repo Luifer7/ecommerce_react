ff41bd9c1066ad41874393a43034ee06e207e768
chrisHuila <christian.huila22@gmail.com>
2023-02-07 17:29:57 -0500

    boton busqueda responsivo

--- src/components/FormularioBusqueda.jsx
+++ src/components/FormularioBusqueda.jsx
@@ -6,7 +6,7 @@ const FormularioBusqueda = () => {
                 <div className="form-group col-md-6 mt-1">
                     <input type="text" className="form-control" placeholder="Busca por Articulo o por Categoría" />
                 </div>    
-                <div className="form-group col-md-1 mt-1">
+                <div className="form-group col-md-1 mt-1 d-flex justify-content-end justify-content-md-start">
                     <input 
                     type="submit" className="btn btn-danger btn-block"
                     value='Buscar'
