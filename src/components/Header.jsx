2be57ecc33cafd1b137f2c7319366cbd77890f52
chrisHuila <christian.huila22@gmail.com>
2023-02-07 17:11:31 -0500

    login y busqueda en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
+
+import { Fragment } from 'react';
+import Login from './Login';
+import FormularioBusqueda from './FormularioBusqueda';
+
 import '../styles/app.css'
+
 
 const Header = () => {
     return ( 
-          <header>
-            <h1 className='text-center text-md-end'> El Comercio</h1>
-          </header>
+        <Fragment>
+          <div className="row mb-4">
+              <div className="d-flex justify-content-end col-md-4 order-md-last">
+                  <Login/>                            
+              </div>
+              <div className="col-md-8 pt-md-3">
+                <header>
+                  <h1 className='text-center text-md-end'> El Comercio</h1>
+                </header>
+              </div>                       
+          </div>
+          <FormularioBusqueda/>
+
+        </Fragment>
+      
+
      )
 }
  
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,24 +1,14 @@
-import { Fragment } from 'react'
-import Header from '../components/Header'
-import Login from '../components/Login'
-import FormularioBusqueda from '../components/FormularioBusqueda'
-import Footer from '../components/Footer'
-import '../styles/routes.css'
+import { Fragment } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../styles/routes.css';
 
 const Home  = () => {
     return ( 
         <Fragment>
             <div className="jumbotron container__Header">
                 <div className="container">
-                    <div className="row mb-4">
-                        <div className="d-flex justify-content-end col-md-4 order-md-last">
-                            <Login/>                            
-                        </div>
-                        <div className="col-md-8 pt-md-3">
-                            <Header/>
-                        </div>                       
-                    </div>
-                    <FormularioBusqueda/>
+                    <Header/>
                 </div>
             </div>
            <div className="vistas">
