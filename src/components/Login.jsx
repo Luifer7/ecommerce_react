6b38a3d11af11bc3e4589184b4c92b89d4605279
chrisHuila <christian.huila22@gmail.com>
2023-02-06 19:04:08 -0500

    probando stylos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,12 @@
-import '../styles/routes.css'
+import '../styles/app.css'
 
 
 const Login = () => {
     return ( 
       <nav className="navbar navbar-expand-md">
-        <div className="collapse navbar-collapse" >
+        <div className="collapse navbar-collapse login__div" >
           <div className="navbar-nav">
-            <a className="nav-link active" href="#">sign in </a>
+            <a className="nav-link active" href="#">sign in </a>            
             <a className="nav-link active" href="#">Log in </a>
           </div>
         </div>
