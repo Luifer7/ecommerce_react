4d7e5e27b84c57e2249343d073b5c91056da9c30
chrisHuila <christian.huila22@gmail.com>
2023-02-22 16:40:56 -0500

    secciones actualizadas

--- src/components/producto/SeccionesProducto.jsx
+++ src/components/producto/SeccionesProducto.jsx
@@ -1,74 +1,24 @@
 import { Fragment, useEffect } from "react";
 import Producto from "./Producto";
 
-const SeccionesProducto = ({productos}) => {
-    // partiendo de categorias como objeto
-    const categorias = {
-        moda: [
-            {id: 1,  mostrar: true, categoria: 'Man & Woman Fashion', nombre: 'Sneakers', imagen: 'https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', precio: 50, comentarios: "lorem ipsu"},
-            {id: 2,  mostrar: true, categoria: 'Man & Woman Fashion', nombre: 'lentes', imagen: 'https://images.pexels.com/photos/701877/pexels-photo-701877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', precio: 50, comentarios: "lorem ipsu"},
-            {id: 3,  mostrar: true, categoria: 'Man & Woman Fashion', nombre: 'joyas', imagen: `https://images.pexels.com/photos/1191531/pexels-photo-1191531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-            
-        ],
-        tecnologia: [
-            {id: 1,  mostrar: true, categoria: 'tecnologia', nombre: 'controller', imagen: `https://images.pexels.com/photos/1666759/pexels-photo-1666759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-            {id: 2,  mostrar: true, categoria: 'tecnologia', nombre: 'controller', imagen: `https://images.pexels.com/photos/1666759/pexels-photo-1666759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-            {id: 3,  mostrar: true, categoria: 'tecnologia', nombre: 'controller', imagen: `https://images.pexels.com/photos/1666759/pexels-photo-1666759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-        ],
-        deportes: [
-            {id: 1,  mostrar: true, categoria: 'deportes', nombre: 'Boxing gloves', imagen: `https://images.pexels.com/photos/5580067/pexels-photo-5580067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-            {id: 2,  mostrar: true, categoria: 'deportes', nombre: 'tennis racket', imagen: `https://images.pexels.com/photos/13464779/pexels-photo-13464779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-            {id: 3,  mostrar: true, categoria: 'deportes', nombre: 'gym', imagen: `https://images.pexels.com/photos/14673247/pexels-photo-14673247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`, precio: 50, comentarios: "lorem ipsu"},
-          
-        ]       
-
-
-    }
-
-    // se extrae de categoria
-    const {moda, tecnologia, deportes} = categorias;
-
+const SeccionesProducto = ({productos, categorias}) => {
+    
     return ( 
         <Fragment>
-            <h2 className="mt-5 headingCategoria">Desde la base de datos</h2>
-            <div className="row mt-3">
-                {productos.map(articulo => ( articulo
-                ? <Producto
-                        key={articulo.id}
-                        articulo={articulo}
-                    />
-                : null))}
-            </div>
-
-            <h2 className="mt-5 headingCategoria">Man & Woman Fashion</h2>
-            <div className="row mt-3">
-                {moda.map(articulo => ( articulo.mostrar 
-                ? <Producto
-                        key={articulo.id}
-                        articulo={articulo}
-                    />
-                : null))}
-            </div>
-
-            <h2 className="mt-5 headingCategoria">Tecnología</h2>
-            <div className="row mt-3">
-                {tecnologia.map(articulo => ( articulo.mostrar 
-                ? <Producto
-                        key={articulo.id}
-                        articulo={articulo}
-                    />
-                : null))}
-            </div>
-
-            <h2 className="mt-5 headingCategoria">Deportes</h2>
-            <div className="row mt-3">
-                {deportes.map(articulo => ( articulo.mostrar 
-                ? <Producto
-                        key={articulo.id}
-                        articulo={articulo}
-                    />
-                : null))}
-            </div>
+            {categorias.map(categoria => (
+                <div className="row mt-5">
+                    <h2 className=" headingCategoria">{categoria}</h2>
+                    <div className="row mt-3">
+                    {productos.map(articulo => ( articulo.producto_categoria === categoria
+                    ?<Producto
+                            key={articulo.id}
+                            articulo={articulo}
+                        />
+                    : null))}
+                    </div> 
+                </div>
+            ))}
+            
 
         </Fragment>
      );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,17 +10,34 @@ const Home  = () => {
 
     // state del componente
     const [productos, guardaProductos] = useState([]);
+    const [ categorias, guardarCategorias] = useState([]);
 
     // trae los productos del custom hook 
     const {bodyDataModule} = useAdmin('productos');
     
     useEffect(() =>{
     if(!bodyDataModule) return <h1>Error en los productos</h1>;
-        // en caso de existir hace disponibles los productos
-        guardaProductos(bodyDataModule);
-        console.log(bodyDataModule)
+        // en caso de existir hace disponibles los productos        
+        try {
+            const productos = bodyDataModule;
+            let categorias = []
+
+            // agrega las categorias
+            for(let i= 0; i < productos.length; i++ ){                
+                if(!categorias.includes(productos[i].producto_categoria)){
+                    categorias.push(productos[i].producto_categoria)
+                }
+            }
+            // pasa los productos y las categorias
+            guardarCategorias(categorias);
+            guardaProductos(productos);
+
+        } catch (error) {
+            console.log('hubo un error con los productos' + error);
+        }
 
     }, [bodyDataModule]);
+    
     return ( 
         <Fragment>
             <Header/>
@@ -28,6 +45,7 @@ const Home  = () => {
            <div className="container">
                 <SeccionesProducto
                     productos={productos}
+                    categorias={categorias}
                 />
            </div>
            
